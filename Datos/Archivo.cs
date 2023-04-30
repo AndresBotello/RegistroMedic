6a0208379cb905591724aaa85025e9a386a107a4
Eder Martínez <Eder Martnez@DESKTOP-6PJAOKL>
2023-04-30 13:52:09 -0500

    Hasta ahora...

---
+++ Datos/Archivo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Archivo
+    {
+        private readonly string rutaArchivo = "Registro medico.txt";
+
+        public void AgregarRegistro(Registro registro) 
+        {
+            using (StreamWriter writer = new StreamWriter(rutaArchivo, true)) 
+            {
+                writer.WriteLine($"{}");
+            }
+        }
+
+    }
+}
