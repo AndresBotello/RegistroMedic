8a9ae7297d3b2cc93fa3cd0c83cc1f355f848c85
ederd <ederd@LAPTOP-DVIJLVRA>
2023-05-18 17:10:02 -0500

    .

--- Datos/Archivo.cs
+++ Datos/Archivo.cs
@@ -16,9 +16,70 @@ namespace Datos
         {
             using (StreamWriter writer = new StreamWriter(rutaArchivo, true)) 
             {
-                writer.WriteLine($"{paciente.Cedula},{paciente.Nombre},{paciente.Telefono},{paciente.Edad},{paciente.Direccion},{paciente.Sexo}");
+                writer.WriteLine($"{paciente.Cedula},{paciente.Nombre},{paciente.Telefono},{paciente.Edad},{paciente.Direccion},{paciente.Sexo}, {paciente.Estrato}" +
+                    $"");
             }
         }
 
+        //public void ActualizarPaciente(Paciente paciente)
+        //{
+        //    string tempArchivo = "temp.txt";
+        //    using (StreamReader sr = new StreamReader(rutaArchivo))
+        //    using (StreamWriter sw = new StreamWriter(tempArchivo))
+        //    {
+        //        string linea;
+        //        while ((linea = sr.ReadLine()) != null)
+        //        {
+        //            string[] campos = linea.Split(',');
+        //            string pacienteNombre = campos[0];
+        //            string pacienteApellido = campos[1];
+
+        //            if (pacienteNombre == nombre && pacienteApellido == apellido)
+        //            {
+        //                // Escribir la línea actualizada en el archivo temporal
+        //                sw.WriteLine($"{nombre},{apellido},{edad}");
+        //            }
+        //            else
+        //            {
+        //                // Escribir las líneas que no requieren actualización en el archivo temporal
+        //                sw.WriteLine(linea);
+        //            }
+        //        }
+        //    }
+
+        //    // Reemplazar el archivo original con el archivo temporal
+        //    File.Delete(rutaArchivo);
+        //    File.Move(tempArchivo, rutaArchivo);
+        //}
+
     }
+
+    //public void EliminarPaciente(string nombre, string apellido)
+    //{
+    //    string tempArchivo = "temp.txt";
+    //    using (StreamReader sr = new StreamReader(rutaArchivo))
+    //    using (StreamWriter sw = new StreamWriter(tempArchivo))
+    //    {
+    //        string linea;
+    //        while ((linea = sr.ReadLine()) != null)
+    //        {
+    //            string[] campos = linea.Split(',');
+    //            string pacienteNombre = campos[0];
+    //            string pacienteApellido = campos[1];
+
+    //            if (pacienteNombre == nombre && pacienteApellido == apellido)
+    //            {
+    //                // Saltar la línea que coincide con el paciente a eliminar
+    //                continue;
+    //            }
+
+    //            // Escribir las líneas que no coinciden en el archivo temporal
+    //            sw.WriteLine(linea);
+    //        }
+    //    }
+
+    //    // Reemplazar el archivo original con el archivo temporal
+    //    File.Delete(rutaArchivo);
+    //    File.Move(tempArchivo, rutaArchivo);
+    //}
 }
