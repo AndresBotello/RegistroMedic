f9b5092b4b8e9114e55ef32672d5a2cc11d65597
Andres Botello <acbotello@unicesar.edu.co>
2023-05-18 22:52:44 -0500

    m

--- Presentacion/FrmPaciente.Designer.cs
+++ Presentacion/FrmPaciente.Designer.cs
@@ -161,7 +161,7 @@
             // 
             // tabPage3
             // 
-            this.tabPage3.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage3.BackColor = System.Drawing.Color.WhiteSmoke;
             this.tabPage3.Controls.Add(this.btnActualizar);
             this.tabPage3.Controls.Add(this.dateTimePicker1);
             this.tabPage3.Controls.Add(this.label8);
@@ -638,7 +638,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(1113, 688);
+            this.tabPage4.Size = new System.Drawing.Size(1113, 648);
             this.tabPage4.TabIndex = 1;
             this.tabPage4.Text = "Datos Historia Clinica";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -648,6 +648,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1191, 690);
             this.Controls.Add(this.tabControl2);
             this.Controls.Add(this.groupBox2);
